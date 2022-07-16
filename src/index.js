5d580d2ad49ca7fd924f5d306b4c3fb1c98a3ee0
Villiteam <nguyenloc24112k3@gmail.com>
2022-07-16 08:24:32 +0700

    Create Route and Init Login, Register, Dashboard

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import Home from './pages/Home/Home';
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
